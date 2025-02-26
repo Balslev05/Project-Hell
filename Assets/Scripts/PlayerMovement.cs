07b28d151c890adb77c95449936bc043a99015f9
ShamiDud <samuelkkristensen@gmail.com>
2025-02-27 00:58:07 +0100

    dodge update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Header("Components")]
     private Rigidbody2D rb;
 
-    [SerializeField] private float speed;
+    private Vector2 playerInput;
+
+    [Header("Stats")]
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float rollSpeed = 10;
+    //[SerializeField] private float rollCooldown = 1f;
+    private bool isMoving;
+    private bool isDodgeing;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isDodgeing = false;
     }
 
     void Update()
     {
         LookAtMouse();
+
+        if (isDodgeing) { return; }
+        GetPlayerInput();
+    }
+
+    void FixedUpdate()
+    {
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isDodgeing)
+            Dodge();
     }
 
     private void LookAtMouse()
@@ -24,9 +43,20 @@ public class PlayerMovement : MonoBehaviour
         transform.up = mousePosition - new Vector2(transform.position.x, transform.position.y);
     }
 
+    public void GetPlayerInput()
+    {
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        playerInput.y = Input.GetAxisRaw("Vertical");
+    }
+
     private void Move()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rb.linearVelocity = input.normalized * speed;
+        rb.linearVelocity = playerInput.normalized * speed;
+    }
+
+    private void Dodge()
+    {
+        isDodgeing = true;
+        rb.linearVelocity = playerInput.normalized * rollSpeed;
     }
 }
