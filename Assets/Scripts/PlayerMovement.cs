416ae6aa59c3c944064324f778f0d17c1e94be60
ShamiDud <samuelkkristensen@gmail.com>
2025-02-27 21:55:01 +0100

    Dodge og invincibility

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,36 +5,50 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Components")]
     private Rigidbody2D rb;
+    private Collider2D collider;
+    private SpriteRenderer spriteRenderer;
 
     private Vector2 playerInput;
 
     [Header("Stats")]
-    [SerializeField] private float speed = 5;
-    [SerializeField] private float rollSpeed = 10;
-    //[SerializeField] private float rollCooldown = 1f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float rollSpeed = 10f;
+    [SerializeField] private float rollDuration = 1f;
+    [SerializeField] private float rollCooldown = 1f;
     private bool isMoving;
+    private bool isRolling;
+    private bool canRoll;
     private bool isDodgeing;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        isDodgeing = false;
+        collider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        isRolling = false;
+        canRoll = true;
+        moveSpeed = walkSpeed;
     }
 
     void Update()
     {
         LookAtMouse();
+        Dodge();
 
-        if (isDodgeing) { return; }
+        if (playerInput != Vector2.zero) { isMoving = true; }
+        else { isMoving = false; }
+
+        if (isRolling) { return; }
         GetPlayerInput();
+
+        if (Input.GetKey(KeyCode.Space) && canRoll && isMoving)
+            StartCoroutine(DodgeRoll());
     }
 
     void FixedUpdate()
     {
         Move();
-
-        if (Input.GetKeyDown(KeyCode.Space) && !isDodgeing)
-            Dodge();
     }
 
     private void LookAtMouse()
@@ -51,12 +65,48 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        rb.linearVelocity = playerInput.normalized * speed;
+        rb.linearVelocity = playerInput.normalized * moveSpeed;
+    }
+
+    private IEnumerator DodgeRoll()
+    {
+        isRolling = true;
+        canRoll = false;
+        moveSpeed = rollSpeed;
+        Invincible(true);
+
+        yield return new WaitForSeconds(rollDuration);
+        isRolling = false;
+        moveSpeed = walkSpeed;
+        Invincible(false);
+
+        yield return new WaitForSeconds(rollCooldown);
+        canRoll = true;
     }
 
     private void Dodge()
     {
-        isDodgeing = true;
-        rb.linearVelocity = playerInput.normalized * rollSpeed;
+        if (Input.GetKeyDown(KeyCode.Q) && !isDodgeing)
+        {
+            Invincible(true);
+            isDodgeing = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && isDodgeing)
+        {
+            Invincible(false);
+            isDodgeing = false;
+        }
+    }
+
+    private void Invincible(bool becomeInvincible)
+    {
+        if (becomeInvincible) {
+            collider.enabled = true;
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.5f);
+        }
+        else if (!becomeInvincible) {
+            collider.enabled = false;
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1f);
+        }
     }
 }
