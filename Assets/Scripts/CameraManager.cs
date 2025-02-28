86a9cd551aaea4aff38dd293b7d45812793007e1
ShamiDud <samuelkkristensen@gmail.com>
2025-02-28 22:55:27 +0100

    CameraFollow update

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,19 +2,33 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    private Vector3 offset;
+    [SerializeField] private Transform player;
+    [SerializeField] private Camera camera;
     [SerializeField] private float smoothTime;
+    [SerializeField] private float threshold;
+    [SerializeField] private GameObject DebugPrefab;
+    GameObject Debug;
+    private Vector3 offset;
     private Vector3 currentVelocity = Vector3.zero;
 
     private void Awake()
     {
-        offset = transform.position - target.position;
+        offset = transform.position - player.position;
+    }
+
+    private void Start()
+    {
+        Debug = Instantiate(DebugPrefab);
     }
 
     private void FixedUpdate()
     {
-        Vector3 targetPosition = target.position + offset;
+        Vector3 targetPosition = (player.position + camera.ScreenToWorldPoint(Input.mousePosition)) / 2f + offset;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -threshold + player.position.x, threshold + player.position.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, -threshold + player.position.y, threshold + player.position.y);
+
+        Debug.transform.position = targetPosition;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
     }
 }
